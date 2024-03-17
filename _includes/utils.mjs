18b388914b242e35674ce5e767bf956d46dbf122
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-03-16 23:36:42 -0400

    doesn't want to work for some reason

---
+++ _includes/utils.mjs
@@ -0,0 +1,18 @@
+
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://uhzanjzmmmltliylrraq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoemFuanptbW1sdGxpeWxycmFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MzAyNzIsImV4cCI6MjAyNjIwNjI3Mn0.TqeD2Ar_0jCTU-CNSl6VcODZUdWUWMoy_Hhs19b0Ocs'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+async function GetBooks(){
+  let { data: books, error } = await supabase
+.from('books')
+.select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books')
+    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.ISBN}</td></tr>`;
+   }
+  }
+  GetBooks();
