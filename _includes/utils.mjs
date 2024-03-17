0c4773552ec57866078098f48dfa007b94406350
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-03-17 01:08:39 -0400

    quick fix

--- _includes/utils.mjs
+++ _includes/utils.mjs
@@ -1,5 +1,5 @@
 
-const { createClient } = require ('@supabase/supabase-js');
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 const supabaseUrl = 'https://uhzanjzmmmltliylrraq.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoemFuanptbW1sdGxpeWxycmFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MzAyNzIsImV4cCI6MjAyNjIwNjI3Mn0.TqeD2Ar_0jCTU-CNSl6VcODZUdWUWMoy_Hhs19b0Ocs'
 const supabase = createClient(supabaseUrl, supabaseKey);
